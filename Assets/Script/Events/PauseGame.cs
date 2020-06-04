3b0af681f4eabc93ab12ab8516bcaa3fae72a52c
Zenith Nelson <raskinzenith@gmail.com>
2020-06-04 18:40:41 +1200

    Intialise Respository
    Created Random Dungeon generator and vuforia setup

---
+++ Assets/Script/Events/PauseGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public GameObject pausePanel;
+
+    void Start()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 0;
+    }
+    void Update()
+    {
+       
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+        //Disable scripts that still work while timescale is set to 0
+    }
+    public void ContinueGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        //enable the scripts again
+    }
+
+}
---
+++ Assets/Script/ProceduralDungeon/DungeonGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DungeonGenerator
+{
+    List<RoomNode> allNodesCollection = new List<RoomNode>();
+    private int dungeonWidth;
+    private int dungeonLength;
+
+    public DungeonGenerator(int dungeonWidth, int dungeonLength)
+    {
+        this.dungeonWidth = dungeonWidth;
+        this.dungeonLength = dungeonLength;
+    }
+
+    public List<Node> CalculateDungeon(
+        int maxIterations, 
+        int roomWidthMin, 
+        int roomLengthMin, 
+        float roomBottomCornerModifier,
+        float roomTopCornerModifier,
+        int roomOffset,
+        int corridorWidth)
+    {
+        //splits the room
+        BinarySpacePartitioner bsp = new BinarySpacePartitioner(dungeonWidth, dungeonLength);
+        allNodesCollection = bsp.PrepareNodesCollection(maxIterations, roomWidthMin, roomLengthMin);
+        List<Node> roomSpaces = StructureHelper.TraverseGraphToExtractLowestLeafes(bsp.RootNode);
+
+        RoomGenerator roomGenerator = new RoomGenerator(maxIterations, roomLengthMin, roomWidthMin);
+        List<RoomNode> roomList = roomGenerator.GenerateRoomsInGivenSpace(roomSpaces,
+        roomBottomCornerModifier,
+        roomTopCornerModifier,
+        roomOffset);
+
+        CorridorGenerator corridorGenerator = new CorridorGenerator();
+        var corridorList = corridorGenerator.CreateCorridor(allNodesCollection, corridorWidth);
+
+   
+        return new List<Node>(roomList).Concat(corridorList).ToList();
+ 
+
+
+    }
+}
