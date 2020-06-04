41d038874b06ced6dfa9f6584f785aa545ad0908
Zenith Nelson <raskinzenith@gmail.com>
2020-06-04 19:18:08 +1200

    Replace Unity Repo

---
+++ Paper_Dungeons/Assets/Script/ProceduralDungeon/Node.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Node
+{
+    //creates universal properties to access in other scripts
+
+    private List<Node> childrenNodeList;
+
+    public List<Node> ChildrenNodeList { get => childrenNodeList; }
+
+    public bool Visited { get; set; }
+
+    public Vector2Int BottomLeftAreaCorner { get; set; }
+    public Vector2Int BottomRightAreaCorner { get; set; }
+    public Vector2Int TopLeftAreaCorner { get; set; }
+    public Vector2Int TopRightAreaCorner { get; set; }
+
+    public Node Parent { get; set; }
+
+    public int TreeLayerIndex { get; set; }
+
+    public Node(Node parentNode)
+    {
+        childrenNodeList = new List<Node>();
+        this.Parent = parentNode;
+        if (parentNode != null)
+        {
+            parentNode.AddChild(this);
+        }
+
+    }
+
+    public void AddChild(Node node)
+    {
+        childrenNodeList.Add(node);
+    }
+
+    public void RemoveChild(Node node)
+    {
+        childrenNodeList.Remove(node);
+    }
+}
---
+++ Paper_Dungeons/Assets/Script/ProceduralDungeon/RoomGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomGenerator
+{
+    private int maxIterations;
+    private int roomLengthMin;
+    private int roomWidthMin;
+
+    public RoomGenerator(int maxIterations, int roomLengthMin, int roomWidthMin)
+    {
+        this.maxIterations = maxIterations;
+        this.roomLengthMin = roomLengthMin;
+        this.roomWidthMin = roomWidthMin;
+    }
+
+    public List<RoomNode> GenerateRoomsInGivenSpace(List<Node> roomSpaces,
+        float roomBottomCornerModifier,
+        float roomTopCornerModifier,
+        int roomOffset)
+    {
+        List<RoomNode> listToReturn = new List<RoomNode>();
+        foreach(var space in roomSpaces)
+        {
+            Vector2Int newBottomLeftPoint = StructureHelper.GenerateBottomLeftCornerBetween(
+                space.BottomLeftAreaCorner, 
+                space.TopRightAreaCorner,
+                0.1f, 1); // change values for increase randomess, closeness of rooms
+
+            Vector2Int newTopRightPoint = StructureHelper.GenerateBottomLeftCornerBetween(
+                space.BottomLeftAreaCorner,
+                space.TopRightAreaCorner,
+                0.9f, 1); // change values for increase randomess, closeness of rooms
+
+            space.BottomLeftAreaCorner = newBottomLeftPoint;
+            space.TopRightAreaCorner = newTopRightPoint;
+            space.BottomRightAreaCorner = new Vector2Int(newTopRightPoint.x, newBottomLeftPoint.y);
+            space.TopLeftAreaCorner = new Vector2Int(newBottomLeftPoint.x, newTopRightPoint.y);
+            listToReturn.Add((RoomNode)space);
+
+        }
+
+        return listToReturn;
+    }
+}
