713118aec39b4ec42dbb7a2a41f3806a9f731821
Zenith Nelson <raskinzenith@gmail.com>
2020-06-10 04:46:23 +1200

    Restarted and retried Procedural
    added style

---
+++ Paper_Dungeons/Assets/Script/ProceduralDungeon2/SpawnRoom.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRoom : MonoBehaviour
+{
+    public LayerMask isThereRoom;
+    public LevelGenerator2 levelGen;
+    // Update is called once per frame
+    void Update()
+    {
+        Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, isThereRoom);
+        if (roomDetection == null && levelGen.stopGeneration == true)
+        {
+            //spawn room random
+            int rand = Random.Range(0, levelGen.rooms.Length);
+            Instantiate(levelGen.rooms[rand], transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+    }
+}
